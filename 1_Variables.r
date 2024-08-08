75175e88f10dfea1c4f5a23c34a31d817e477407
SebasGS <Sebasguesa@hotmail.com>
2024-08-08 07:56:01 -0500

    Primer codigo en R

---
+++ 1_Variables.r
@@ -0,0 +1,24 @@
+#Tema 1 de lenguage de programacion R, declaracion y operacion entre variables
+#Creacion de variables de texto, binarios y numerico
+nombre <- "sebas"
+respuesta <- 1>2
+resultado <- 0
+#Ver que tipo de variable es
+class(resultado)
+class(nombre)
+class(respuesta)
+
+
+#Declaracion de variables numericas
+manzana <- 3
+pera <- 7
+banano <- 9
+# Se crea la operacion
+resultado <- 2*banano + 6*pera + 1*manzana
+#Se muestra en pantalla el resultado
+resultado
+
+
+
+
+
